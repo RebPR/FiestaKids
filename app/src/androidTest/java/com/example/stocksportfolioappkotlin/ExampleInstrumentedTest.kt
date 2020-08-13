bd20e1be2bcf9ea6065fdc5ceb7c70151bbac7a1
RebPR <108851048+RebPR@users.noreply.github.com>
2020-08-13 18:05:11 +0000

    Synchrophasor: Updated code library dependencies.

--- app/src/androidTest/java/com/example/stocksportfolioappkotlin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/stocksportfolioappkotlin/ExampleInstrumentedTest.kt
@@ -1,4 +1,7 @@
 package com.example.stocksportfolioappkotlin
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.A
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+i
