a741f054eef96e66ab4365ad0eef595e11e54e58
RebPR <108851048+RebPR@users.noreply.github.com>
2020-09-26 16:56:38 +0000

    Ops

--- app/src/main/java/com/example/stocksportfolioappkotlin/Constants.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/Constants.kt
@@ -3,4 +3,4 @@ package com.example.stocksportfolioappkotlin
 class Constants {
     companion object{
         const val STOCKS_FULL_LIST="stocksFullListFragment"
-        const val STOCKS_MALFORM
+        const val STOCKS_MALFORMED_LIST="stocksMalfo
