3cb82468d861160fc3108be6d49d997f8b49d54b
RebPR <108851048+RebPR@users.noreply.github.com>
2020-09-27 12:26:04 +0000

    removed tabs

--- app/src/main/java/com/example/stocksportfolioappkotlin/Constants.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/Constants.kt
@@ -4,4 +4,4 @@ class Constants {
     companion object{
         const val STOCKS_FULL_LIST="stocksFullListFragment"
         const val STOCKS_MALFORMED_LIST="stocksMalformedListFragment"
-       
+        const val STOCKS_EMPTY_LIST
