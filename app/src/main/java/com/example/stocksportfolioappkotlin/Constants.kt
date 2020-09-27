e30dd30984a4ccc9083787aa95fd34c5ad0b9e11
RebPR <108851048+RebPR@users.noreply.github.com>
2020-09-27 19:48:42 +0000

    Automated merge with ssh://jfxsrc.us.oracle.com//javafx/8.0/scrum/graphics/jfx//rt

--- app/src/main/java/com/example/stocksportfolioappkotlin/Constants.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/Constants.kt
@@ -4,4 +4,5 @@ class Constants {
     companion object{
         const val STOCKS_FULL_LIST="stocksFullListFragment"
         const val STOCKS_MALFORMED_LIST="stocksMalformedListFragment"
-        const val STOCKS_EMPTY_LIST="stocksEmptyListFragmen
+        const val STOCKS_EMPTY_LIST="stocksEmptyListFragment"
+        const val FRAGM
