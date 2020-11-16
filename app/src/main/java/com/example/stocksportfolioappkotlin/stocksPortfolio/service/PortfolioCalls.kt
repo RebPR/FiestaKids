e60df9dc918ebcff27575c00a526af19ca959706
RebPR <108851048+RebPR@users.noreply.github.com>
2020-11-16 06:35:00 +0000

    - WL#6560: merge from mysql-trunk

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/service/PortfolioCalls.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/service/PortfolioCalls.kt
@@ -12,4 +12,4 @@ interface PortfolioCalls {
     fun getPortfolioStocksFullList(): Call<PortfolioResponseModel>
 
     @GET("/cash-homework/cash-stocks-api/portfolio_malformed.json")
-    fun getPortfolioStocksMalforme
+    fun getPortfolioStocksMalformedList(): Call<PortfolioRespons
