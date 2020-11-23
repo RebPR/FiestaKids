55bbfc01ce8ce2af218f821db5cb15690e8ea700
RebPR <108851048+RebPR@users.noreply.github.com>
2020-11-23 21:50:01 +0000

    Fix uninitialised variable.

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/service/PortfolioCalls.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/service/PortfolioCalls.kt
@@ -15,4 +15,8 @@ interface PortfolioCalls {
     fun getPortfolioStocksMalformedList(): Call<PortfolioResponseModel>
 
     @GET("/cash-homework/cash-stocks-api/portfolio_empty.json")
-    fun getPortfolioStocksEmptyList(): Call<PortfolioRe
+    fun getPortfolioStocksEmptyList(): Call<PortfolioResponseModel>
+
+
+
+    companion
