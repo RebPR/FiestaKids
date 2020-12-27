fabd5c80c7f4fdf4ee0adef8a7524c2647be9028
RebPR <108851048+RebPR@users.noreply.github.com>
2020-12-27 06:42:27 +0000

    Fix #36

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/service/PortfolioRepoService.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/service/PortfolioRepoService.kt
@@ -22,3 +22,34 @@ class PortfolioRepoService: PortfolioRepoInterface {
     private val portfolioCalls = PortfolioCalls.create()
 
     var portfolioListResourceHolder: MutableLiveData<PortfolioResponseModel?> = MutableLiveData()
+    var portfolioMalformedListResourceHolder: MutableLiveData<PortfolioResponseModel?> = MutableLiveData()
+    var portfolioEmptyListResourceHolder: MutableLiveData<PortfolioResponseModel?> = MutableLiveData()
+
+    override fun getPortfolioStocksFullList(): MutableLiveData<PortfolioResponseModel?> {
+        portfolioCalls.getPortfolioStocksFullList()
+            .enqueue(object : Callback<PortfolioResponseModel> {
+                override fun onResponse(
+                    call: Call<PortfolioResponseModel>,
+                    response: Response<PortfolioResponseModel>
+                ) {
+                    portfolioListResourceHolder.value = response.body()
+                    Log.d(TAG, response.body().toString())
+                }
+
+                override fun onFailure(call: Call<PortfolioResponseModel>, t: Throwable) {
+                    portfolioMalformedListResourceHolder.value = null
+                }
+            })
+        return portfolioListResourceHolder
+    }
+
+    override fun getPortfolioStocksMalformedList(): MutableLiveData<PortfolioResponseModel?> {
+        portfolioCalls.getPortfolioStocksMalformedList().enqueue(object : Callback<PortfolioResponseModel> {
+            override fun onResponse(call: Call<PortfolioResponseModel>, response: Response<PortfolioResponseModel>) {
+                portfolioMalformedListResourceHolder.value= response.body()
+                Log.d(TAG,response.body().toString())
+            }
+
+            override fun onFailure(call: Call<PortfolioResponseModel>, t: Throwable) {
+                portfolioMalformedListResourceHolder.value=null
+            }
