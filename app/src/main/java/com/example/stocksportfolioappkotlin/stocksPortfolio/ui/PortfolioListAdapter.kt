a9c716707c1295ca4fcd080e7295115a2d399bd1
RebPR <108851048+RebPR@users.noreply.github.com>
2021-01-03 06:49:26 +0000

    chore(package): update flow-bin to version 0.44.0

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/PortfolioListAdapter.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/PortfolioListAdapter.kt
@@ -33,3 +33,27 @@ class PortfolioListAdapter(private val stocks: List<Stock?>?): RecyclerView.Adap
             }"
             holder.quantity.text = "Stocks owned: ${stocks?.get(position)?.quantity?.let { it.toString()}?: kotlin.run { "0" }}"
             holder.current_price_timestamp.text ="TIme: ${stocks?.get(position)?.current_price_timestamp.toString()}"
+            holder.stockCard.setOnClickListener {
+                if(holder.group.isVisible) {
+                    holder.group.visibility=View.GONE
+                }else{
+                    holder.group.visibility=View.VISIBLE
+                }
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return stocks?.let { return it.size }?: kotlin.run { return 0 }
+        }
+
+        class PortfolioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var ticker: TextView = itemView.findViewById(R.id.ticker)
+            var name: TextView = itemView.findViewById(R.id.name)
+            var currency: TextView = itemView.findViewById(R.id.currency)
+            var current_price_cents: TextView = itemView.findViewById(R.id.current_price_cents)
+            var quantity: TextView = itemView.findViewById(R.id.quantity)
+            var current_price_timestamp: TextView = itemView.findViewById(R.id.current_price_timestamp)
+            var stockCard:CardView =itemView.findViewById(R.id.stockCard)
+            var group: Group =itemView.findViewById(R.id.group)
+        }
+}
