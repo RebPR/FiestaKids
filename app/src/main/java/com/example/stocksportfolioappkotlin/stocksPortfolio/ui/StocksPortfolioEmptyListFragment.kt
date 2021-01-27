51ad0937c69fedcc1f2aae238731a7876c55fab9
RebPR <108851048+RebPR@users.noreply.github.com>
2021-01-27 14:17:35 +0000

    update copyright notices

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
@@ -7,4 +7,4 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.stocksportfolioappkotlin.FragmentBase
 import com.example.stocksportfolioappkotlin.databinding.FragmentStocksPortfolioFullListBinding
-import com.example.stocks
+import com.example.stocksportfolioappkotlin.stocksPort
