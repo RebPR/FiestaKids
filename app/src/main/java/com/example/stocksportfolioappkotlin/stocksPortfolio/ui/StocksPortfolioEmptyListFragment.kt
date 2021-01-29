44e8f325746a894167022976dd96128a960e5183
RebPR <108851048+RebPR@users.noreply.github.com>
2021-01-29 23:05:55 +0000

    Errors

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
@@ -7,4 +7,5 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.stocksportfolioappkotlin.FragmentBase
 import com.example.stocksportfolioappkotlin.databinding.FragmentStocksPortfolioFullListBinding
-import com.example.stocksportfolioappkotlin.stocksPort
+import com.example.stocksportfolioappkotlin.stocksPortfolio.model.responseModels.Stock
+i
