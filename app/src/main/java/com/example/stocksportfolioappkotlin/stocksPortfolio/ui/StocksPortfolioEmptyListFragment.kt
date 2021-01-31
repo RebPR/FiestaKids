c47a2a85d7ee38ffc74387d162c3ee35c3aaf809
RebPR <108851048+RebPR@users.noreply.github.com>
2021-01-31 13:25:33 +0000

    add linter

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
@@ -8,4 +8,4 @@ import androidx.fragment.app.viewModels
 import com.example.stocksportfolioappkotlin.FragmentBase
 import com.example.stocksportfolioappkotlin.databinding.FragmentStocksPortfolioFullListBinding
 import com.example.stocksportfolioappkotlin.stocksPortfolio.model.responseModels.Stock
-import com.example.stocksportfolioappkotlin.
+import com.example.stocksportfolioappkotlin.stocksPortfolio.viewmodel.Port
