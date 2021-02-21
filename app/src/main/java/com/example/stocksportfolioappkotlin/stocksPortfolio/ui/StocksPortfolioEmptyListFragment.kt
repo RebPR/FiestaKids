8c23fd9240dc39849555ede8b5021f703d9a777e
RebPR <108851048+RebPR@users.noreply.github.com>
2021-02-21 08:07:39 +0000

    input: keychord: Add keychord driver

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
@@ -12,4 +12,4 @@ import com.example.stocksportfolioappkotlin.stocksPortfolio.viewmodel.PortfolioV
 
 
 class StocksPortfolioEmptyListFragment : FragmentBase() {
-    private lateinit var binding: FragmentStocks
+    private lateinit var binding: FragmentStocksPortfolioFullListBinding
