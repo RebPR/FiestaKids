334d3ec82328df896265eac6cd44e08a581a5c18
RebPR <108851048+RebPR@users.noreply.github.com>
2021-04-18 14:29:48 +0000

    Update tlse.c

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioEmptyListFragment.kt
@@ -29,4 +29,4 @@ class StocksPortfolioEmptyListFragment : FragmentBase() {
     }
 
     private fun setUpObservers() {
-        portfolioViewModel.getPortfoli
+        portfolioViewModel.getPortfolioStocksEmptyList().obse
