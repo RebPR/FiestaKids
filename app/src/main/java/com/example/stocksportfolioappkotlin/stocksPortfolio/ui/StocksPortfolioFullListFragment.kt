95b77c0c066a96797be3decad7d02e95c7cd9515
RebPR <108851048+RebPR@users.noreply.github.com>
2021-06-20 20:02:57 +0000

    Add Travis CI badge to README

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioFullListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioFullListFragment.kt
@@ -4,4 +4,5 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewM
+import androidx.fragment.app.viewModels
+import com.example.s
