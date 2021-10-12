b56e7aa67d213f40397e37673e603710c571a640
RebPR <108851048+RebPR@users.noreply.github.com>
2021-10-12 15:01:56 +0000

    Update Ejercicio.md

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioListActivity.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioListActivity.kt
@@ -8,4 +8,4 @@ import com.example.stocksportfolioappkotlin.Constants.Companion.STOCKS_FULL_LIST
 import com.example.stocksportfolioappkotlin.Constants.Companion.STOCKS_MALFORMED_LIST
 import com.example.stocksportfolioappkotlin.databinding.ActivityStocksPortfolioListBinding
 
-class S
+class StocksPortfolioListActivity : BaseAct
