d23cef8a11f1a029e1d2e9541c3f8100e495df72
RebPR <108851048+RebPR@users.noreply.github.com>
2021-12-19 05:55:11 +0000

    intelli-thermal: increase sampling time to reduce sampling overhead

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioMalformedListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioMalformedListFragment.kt
@@ -7,4 +7,4 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.stocksportfolioappkotlin.FragmentBase
 import com.example.stocksportfolioappkotlin.databinding.FragmentStocksPortfolioFullListBinding
-import com.
+import com.example.stocksportfolioappkotlin
