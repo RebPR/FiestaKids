3ddd63017d0fa7ef0c92a28e79b40da299f7d7c1
RebPR <108851048+RebPR@users.noreply.github.com>
2022-03-02 14:18:56 +0000

    Pontoon: Update Indonesian (id) localization of AMO Frontend

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioMalformedListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioMalformedListFragment.kt
@@ -42,4 +42,5 @@ class StocksPortfolioMalformedListFragment : FragmentBase() {
         setupList(list,binding.noStocks,binding.stocksFullListRV)
     }
 
-    companion ob
+    companion object {
+        fun newInstance
