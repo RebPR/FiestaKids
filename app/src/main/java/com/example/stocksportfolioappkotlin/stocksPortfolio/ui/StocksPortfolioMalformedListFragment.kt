71e9095ee54697a77488bdf66901c5c51f5b1af8
RebPR <108851048+RebPR@users.noreply.github.com>
2022-03-03 19:16:31 +0000

    bonding: Remove debug_fs files when module init fails

--- app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioMalformedListFragment.kt
+++ app/src/main/java/com/example/stocksportfolioappkotlin/stocksPortfolio/ui/StocksPortfolioMalformedListFragment.kt
@@ -43,4 +43,4 @@ class StocksPortfolioMalformedListFragment : FragmentBase() {
     }
 
     companion object {
-        fun newInstance(): StocksPortfolioMalf
+        fun newInstance(): StocksPortfolioMalformedListFragment = Sto
